a56b32aa3fe57ead52e388c37c54ca2d8daa8aa9
CallMeJunHong <junhong051219@gmail.com>
2024-04-21 21:25:53 +0800

    Changed Booking Table

--- websystem/booking.php
+++ websystem/booking.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html>
     <head>
@@ -15,22 +14,20 @@
             $time = $_POST['time'];
             $people = $_POST['people'];
 
-            header("Location: selecttable.html");
+            header("Location: payment.php");
             exit();
         }
         ?>
     
     <body>
-
-    <div class="nav">
+        <div class="nav">
             <nav>
                 <ul>
                     <li><a href="index.php"><img class="logo"src="image/Snookerlogo.png" alt="Logo"></a></li>        
                     <li><a href="index.php"><h4>Home</h4></li></a>
-                    <li><a href="bookingstatus.php"><h4>Booking Status</h4></li></a>                                
-                    <li><a href="event.php"><h4>Event Booking</h4></li></a>
+                    <li><a href="bookingstatus"><h4>Booking Status</h4></li></a>                                
+                    <li><a href="payment.php"><h4>Payment</h4></li></a>
                     <li><a href="booking.php"><h4>Booking</h4></li></a>
-
                     <div class="right">
                         <div class="searchbar">
                             <form> 
@@ -38,32 +35,42 @@
                             </form>
                         </div>
                         <a class="login-link" href="#login">
-                         <a href="login-user.php"><img class="login-icon" src="login.png" alt="Login"></a>
+                         <a href="login.php"><img class="login-icon" src="login.png" alt="Login"></a>
                         </a>
-                    </div>      
+                    </div>             
                 </ul>
-            </nav>        
+            </nav>
         </div>
 
             <div class="reservation-form">
                 <div class="left-container">
-                    <div class="left-up-column">
-                        <label for="operation">Opening Hour:</label>
-                        <p class="policy"> Monday: 9:00 AM - 10:00 PM<br>
-                            Tuesday: 9.00 AM - 10:00 PM<br>
-                            Wednesday:9.00 AM - 10:00 PM<br>
-                            Thursday: 9.00 AM - 10:00 PM<br>
-                            Friday: 9.00 AM - 10:00 PM<br>
-                            Saturday: 10.00 AM - 11:30 PM<br>
-                            Sunday: 10.00 AM - 11:30PM
-                        </p>
-      
+
+                        <div class="tablediv">
+                            <img src="tables.png" class="tablepic">
+                          </div>
+                        
+                    <div class="left-down-column2">
+                        <details>
+                            <summary>Opening Hour</summary>
+                            <p class="policy"> Monday: 9:00 AM - 10:00 PM<br>
+                                Tuesday: 9.00 AM - 10:00 PM<br>
+                                Wednesday:9.00 AM - 10:00 PM<br>
+                                Thursday: 9.00 AM - 10:00 PM<br>
+                                Friday: 9.00 AM - 10:00 PM<br>
+                                Saturday: 10.00 AM - 11:30 PM<br>
+                                Sunday: 10.00 AM - 11:30PM
+                            </p>
+                        </details>
                       </div>
       
-                      <div class="left-down-column">
-                          <label for="price">Pricing</label>
-                        <p class="policy">1 hour: RM10<br>
+                      <div class="left-down-column2">
+                      <details>
+                            <summary>Price</summary>
+                            <p class="policy">
+                            1 hour: RM10<br>
                             2 hours: RM15</p>
+                            </p>
+                        </details>
                       </div>
 
                       <div class="left-down-column2">
@@ -82,7 +89,7 @@
                 <div class="right-container">
                     <div class="right-column">
 
-                        <form action="selecttable.php" method="post">
+                        <form action="payment.php" method="post">
                         <h3 class="policy">Select date and time for your reservation</h3>
                             <label>Date:</label>
                             <input type="date" id="date" name="date" placeholder="mm/dd/yyyy" required>
@@ -91,7 +98,31 @@
                             <label>Duration:</label>
                             <input type="range" id="duration" name="duration" min="1" max="5" value="1" step="1" required> 
                             <output for="duration" id="durationOutput">1 hour</output>
-                            <script>    
+
+                            <h3 class="policy">Please Select a Table</h3>
+                            <div class="row">
+                                <input  type="radio" id="option1" name="option" value="option1" required>
+                                <label for="option1">Table 1</label>
+                                <input type="radio" id="option2" name="option" value="option2">
+                                <label for="option2">Table 2</label>
+                                <input type="radio" id="option3" name="option" value="option3">
+                                <label for="option3">Table 3</label>
+                                <input type="radio" id="option4" name="option" value="option4">
+                                <label for="option4">Table 4</label>
+                            </div>
+                            <div class="row">
+                                <input type="radio" id="option5" name="option" value="option5">
+                                <label for="option5">Table 5</label>
+                                <input type="radio" id="option6" name="option" value="option6">
+                                <label for="option6">Table 6</label>
+                                <input type="radio" id="option7" name="option" value="option7">
+                                <label for="option7">Table 7</label>
+                                <input type="radio" id="option8" name="option" value="option8">
+                                <label for="option8">Table 8</label>
+                            </div>
+                            
+                            
+                            <script>
                             const durationInput = document.getElementById('duration');
                             const output = document.getElementById('durationOutput');
 
@@ -101,11 +132,13 @@
                             </script>
 
                             <input id="submit" type="submit" name="submit" value="Check Availability">
+                            <input id="back" type="button" value="< Back" onclick="window.location.href='index.php'">
                         </form>
                     </div>
                 </div>
                 
               </div>
         </div>
-    </body>
+    </body>
 </html>
+
